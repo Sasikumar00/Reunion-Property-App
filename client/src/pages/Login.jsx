81732da33bdd92a0f0defb6935d28157b4d5d0fe
Sasikumar .M <sasikumarworks@gmail.com>
2023-10-01 04:26:51 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -29,7 +29,7 @@ const Login = () => {
     <Layout>
     <h1 className='text-center text-5xl mt-5'>Log In</h1>
     <div className='flex items-center justify-center min-h-[70vh]'>
-        <form onSubmit={handleSubmit} method='post' className='h-full w-[50%] border border-[#051923] p-3 rounded-md'>
+        <form onSubmit={handleSubmit} method='post' className='h-full w-[80%] md:w-[50%] border border-[#051923] p-3 rounded-md'>
         <label className='input-label flex-col'>
             Email Address
             <input type="email" className='border-2 rounded-md px-5 py-1 outline-none' name='email' placeholder='Enter email' onChange={(e)=>{setUserData({...userData,'email': e.target.value})}} required/>
@@ -39,7 +39,7 @@ const Login = () => {
             <input type="password" className='border-2 rounded-md px-5 py-1 outline-none' name='password' placeholder='Enter password' onChange={(e)=>{setUserData({...userData,'password': e.target.value})}} required/>
         </label>
         <div className='flex items-center justify-center'>
-        <input value="Login" type="submit" className='bg-[#03C988] hover:bg-[#02B47A] transition-all ease-in-out duration-300 w-[50%] mt-5 py-2 text-xl text-white rounded-md cursor-pointer'/>
+        <input value="Login" type="submit" className='bg-[#03C988] hover:bg-[#02B47A] transition-all ease-in-out duration-300 w-[80%] md:w-[50%] mt-5 py-2 text-xl text-white rounded-md cursor-pointer'/>
         </div>
         </form>
     </div>
